96b7e395ff8b6b56236ecf1b7a817c9bd5464933
First Pasit <giccer11@gmail.com>
2024-01-24 14:24:43 +0700

    First commit

---
+++ src/main/java/com/example/ex2bb/Ex2bbApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex2bb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex2bbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex2bbApplication.class, args);
+    }
+
+}
